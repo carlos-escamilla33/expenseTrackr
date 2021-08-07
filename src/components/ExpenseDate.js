3a83edc1a7654e6008debea0eac0852b5a3c3ed7
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2021-08-06 22:51:55 -0700

    added ExpenseDate component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "./ExpenseDate.css";
 
 const ExpenseDate = (props) => {
+    const month = props.date.toLocaleString("en-US", {month: "long"});
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString("en-US", { day : "2-digit" });
     return (
-        <div>
-            <div>{props.month}</div>
-            <div>{props.year}</div>
-            <div>{props.day}</div>
+        <div className="expense-date">
+            <div className="expense-date__month">{month}</div>
+            <div className="exepense-date__year">{year}</div>
+            <div className="expense-date__day">{day}</div>
         </div>
     )
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -10,16 +10,13 @@ the css file into the JS file and after that just link of the classNames with
 the CSS file just like normal HTML + CSS*/
 // WE WANT TO BE ABLE TO REUSE OUR COMPONENTS
 import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate";
+import ExpenseDate from "./ExpenseDate.js";
 
 
 const ExpenseItem = (props) => {
-    const month = props.date.toLocaleString("en-US", {month: "long"});
-    const year = props.date.getFullYear();
-    const day = props.date.toLocaleString("en-US", { day : "2-digit" });
     return (
         <div className = "expense-item">
-            <ExpenseDate month={month} year={year} day={day}/>
+            <ExpenseDate date={props.date}/>
             <div className = "expense-item__description">
                 <h2>{props.title}</h2>
                 <div className = "expense-item__price">$ {props.amount}</div>
